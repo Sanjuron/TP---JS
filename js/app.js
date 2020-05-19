fefe18cc6bd9931c2830da55e32138ca3c3cd881
sanjuron <julien_nn@live.fr>
2020-05-19 15:18:54 +0200

    avant la génération de la couleur aléatoire

---
+++ js/app.js
@@ -0,0 +1,31 @@
+let norrisFacts = [
+    "Chuck Norris a déjà compté jusqu'à l'infini. Deux fois.",
+    "Google, c'est le seul endroit où tu peux taper Chuck Norris..",
+    "Certaines personnes portent un pyjama Superman. Superman porte un pyjama Chuck Norris",
+    "Chuck Norris donne fréquemment du sang à la Croix-Rouge. Mais jamais le sien.",
+    "Chuck Norris et Superman ont fait un bras de fer, le perdant devait mettre son slip par dessus son pantalon.",
+    "Chuck Norris ne se mouille pas, c'est l'eau qui se Chuck Norris.",
+    "Chuck Norris peut gagner une partie de puissance 4 en trois coups.",
+    "Jesus Christ est né en 1940 avant Chuck Norris.",
+    "Chuck Norris ne porte pas de montre. Il décide de l'heure qu'il est.",
+    "La seule chose qui arrive à la cheville de Chuck Norris... c'est sa chaussette.",
+    "Chuck Norris fait pleurer les oignons.",
+    "Chuck Norris peut diviser par zéro.",
+    "Chuck Norris comprend Jean-Claude Van Damme.",
+];
+
+console.log(norrisFacts);
+
+function randFacts(){
+    let rand = Math.floor(Math.random() * norrisFacts.length) // génère nombre aléatoire qui prend l'entièreté du tableau
+    document.getElementById("quote").innerHTML = norrisFacts[rand]; // assigne les citations aléatoires dans p
+}
+
+console.log(randFacts());
+
+let button = document.getElementById("button"); 
+button.addEventListener("click", randFacts); // ! ne surtout pas oublier les parenthèses autour de click! Et ne pas en mettre après la fonction!!
+
+function randColor(){
+    
+}
